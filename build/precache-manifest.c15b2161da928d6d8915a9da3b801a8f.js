800909111e84997727afe20e3a058578df19ef19
pratiksutar97 <pratikdaz1997@gmail.com>
2020-10-23 13:13:29 +0530

    Update

---
+++ build/precache-manifest.c15b2161da928d6d8915a9da3b801a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e780509a376227b78b8bc9b28754e99f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d41e150467b768ac58",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b1cf4c90c616e8e3ccac",
+    "url": "/static/js/2.1e4a86a2.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.1e4a86a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d41e150467b768ac58",
+    "url": "/static/js/main.cbd9f344.chunk.js"
+  },
+  {
+    "revision": "7237a816ca3cf3922cf1",
+    "url": "/static/js/runtime-main.702a33e6.js"
+  }
+]);
