a260f07b41e0b340b30b2871fd4d2a9033c9eb04
Pratik Sutar <pratikdaz1997@gmail.com>
2020-11-26 23:19:04 -0800

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,7 +17,10 @@ class Footer extends Component {
     return (
       <footer>
         <div className="row">
-          
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
+            
+          </div>
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
               <i className="icon-up-open"></i>
