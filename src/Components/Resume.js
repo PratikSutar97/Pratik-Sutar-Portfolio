096259a0d0be4cd2d9946c1e86677a54ab5e76a5
pratiksutar97 <pratikdaz1997@gmail.com>
2025-02-01 16:30:29 +0530

    experience added

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,6 +5,14 @@ class Resume extends Component {
 
     if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
+      var experience = this.props.data.experience.map(function (experience) {
+        return <div key={experience.company}><h3>{experience.company}</h3>
+          <p className="info">{experience.title}<span>&bull;</span> <em className="date">{experience.years}</em></p>
+          <p>
+            {experience.description}
+          </p>
+        </div>
+      })
       var education = this.props.data.education.map(function (education) {
         return <div key={education.school}><h3>{education.school}</h3>
           <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -18,7 +26,16 @@ class Resume extends Component {
 
     return (
       <section id="resume">
+      
+        <div className="row experience">
+          <div className="three columns header-col">
+            <h1><span>Experience</span></h1>
+          </div>
 
+          <div className="nine columns main-col">
+            {experience}
+          </div>
+        </div>
         <div className="row education">
           <div className="three columns header-col">
             <h1><span>Education</span></h1>
