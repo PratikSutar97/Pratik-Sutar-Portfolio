b2a6408fcf0f971f1ad0e21928b8f5b2e0732573
pratiksutar97 <pratikdaz1997@gmail.com>
2025-02-25 12:32:09 +0530

    Update fonts resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -34,7 +34,7 @@ class Header extends Component {
 
             <div className="row banner">
                <div className="banner-text">
-                  <h1 className="responsive-headline"> {name}.</h1>
+                  <h2 className="responsive-headline"> {name}</h2>
                   <h3>I'm a {city} based <span>{occupation}</span> {description}</h3>
                   <hr />
                   <ul className="social">
